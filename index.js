5b89754a69d3b39e4c8834c3c0b0e2da4448519a
yves848 <yves.godart@gmail.com>
2023-11-02 13:19:13 +0000

    switch to pool

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ import express from 'express';
 import pkg from 'pg';
 import fs from 'fs';
 
+const pool = new pkg.Pool({
+  user: 'postgres',
+    host: '192.168.50.20',
+    database: 'sleevetrack',
+    password: 'daeGh@id379@@'
+});
+
 const { Client } = pkg;
 
 const PORT = process.env.PORT || 3001;
@@ -14,64 +21,35 @@ app.get("/api", (req,res)=> {
 
 
 app.get('/evolution', async (req,res) => {
-  const client = new Client({
-    user: 'postgres',
-    host: '192.168.50.20',
-    database: 'sleevetrack',
-    password: 'daeGh@id379@@'
-  })
-  await client.connect();
-
-  const q = await client.query('select * from evolution');
+  const q = await pool.query('select * from evolution');
   if (q.rowCount > 0) {
     // console.log(q);
     res.json({rows : q.rows});
   }
-  await client.end();
 });
 
 app.get('/insert', async (req,res) => {
   const date = req.query.date;
   const poids = req.query.poids;
-  const client = new Client({
-    user: 'postgres',
-    host: '192.168.50.20',
-    database: 'sleevetrack',
-    password: 'daeGh@id379@@'
-  })
-  await client.connect();
   const query = 'insert into evolution (date,poids) values ($1,$2)';
   const values = [date,poids];
-  const q = await client.query(query,values);
-  await client.end();
+  const q = await pool.query(query,values);
   console.log("insert",date,poids);
   res.send({message : 'ok'});
-  
 });
 
 app.get('/delete', async(req,res) => {
   const id = req.query.id.trim();
   console.log('delete id ',id);
-  const client = new Client({
-    user: 'postgres',
-    host: '192.168.50.20',
-    database: 'sleevetrack',
-    password: 'daeGh@id379@@'
-  })
   const query = 'delete from evolution where id IN ($1)';
   const values = [id];
   try {
-    // console.log('query');
-    await client.connect();
-    const q = await client.query(query,values);
+    const q = await pool.query(query,values);
   }
   catch (e) {
     console.error(e);
   }
-  
   res.send({message: `id : ${id} deleted`})
-  await client.end();
-  
 });
 
 app.listen(PORT, () => {
